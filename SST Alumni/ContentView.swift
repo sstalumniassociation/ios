8521f7081d4da56d18274d2ae99e67e152041163
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-16 00:12:01 +0800

    Initial Commit

---
+++ SST Alumni/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SST Alumni
+//
+//  Created by Jia Chen Yee on 16/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SST Alumni/SST_AlumniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SST_AlumniApp.swift
+//  SST Alumni
+//
+//  Created by Jia Chen Yee on 16/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SST_AlumniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
