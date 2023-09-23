e4c48f039d6bf85188f17a26f0e2c716c984a7f7
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-23 20:22:36 +0800

    fix: remove previous event code in Access Code field

--- SST Alumni/Views/SSTAARS/SSTAARSView.swift
+++ SST Alumni/Views/SSTAARS/SSTAARSView.swift
@@ -31,6 +31,7 @@ struct SSTAARSView: View {
                         }
                     }
                     Button("Enter Access Code") {
+                        eventCode = ""
                         accessCodeAlertPresented.toggle()
                     }
                 }
