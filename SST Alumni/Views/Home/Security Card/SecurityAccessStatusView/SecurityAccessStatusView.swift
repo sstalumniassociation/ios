387763eeca8e4852662d289da3fa068638f7e8cf
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-25 03:37:48 +0800

    feat: hide hexagon and confetti animations when screen is recorded

--- SST Alumni/Views/Home/Security Card/SecurityAccessStatusView/SecurityAccessStatusView.swift
+++ SST Alumni/Views/Home/Security Card/SecurityAccessStatusView/SecurityAccessStatusView.swift
@@ -6,30 +6,34 @@
 //
 
 import SwiftUI
+import ScreenCaptureRedaction
 
 struct SecurityAccessStatusView: View {
     
     @ObservedObject var securityAccessManager: SecurityAccessManager
     
     var body: some View {
-        ZStack {
+        ScreenCaptureRedactionView {
             HexagonView()
                 .drawingGroup()
                 .opacity(0.5)
-                .mask {
-                    RoundedRectangle(cornerRadius: 8)
-                }
-                .overlay {
-                    switch securityAccessManager.securityAccessState {
-                    case .admitted:
-                        SecurityAccessAdmittedView(securityAccessManager: securityAccessManager)
-                    case .denied(let reason):
-                        SecurityAccessDeniedView(reason: reason)
-                    case .processing:
-                        ProgressView()
-                            .tint(.black)
-                    }
-                }
+        } replacingWith: {
+            Rectangle()
+                .fill(.clear)
+        }
+        .mask {
+            RoundedRectangle(cornerRadius: 8)
+        }
+        .overlay {
+            switch securityAccessManager.securityAccessState {
+            case .admitted:
+                SecurityAccessAdmittedView(securityAccessManager: securityAccessManager)
+            case .denied(let reason):
+                SecurityAccessDeniedView(reason: reason)
+            case .processing:
+                ProgressView()
+                    .tint(.black)
+            }
         }
         .background(.white)
         .aspectRatio(1, contentMode: .fit)
